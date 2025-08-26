8cf978c2f75dc425858877b5e471f4c864071ca3
DESKTOP-Q8QHBTM\takag <takagi@m-craft.com>
2025-08-26 17:17:14 +0900

    タイトルシーン・タイトルマネージャの追加 ゲームシーン名の変更

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// TitleScene を管理するクラス
+/// </summary>
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+
+    void Start()
+    {
+        if (startButton != null) startButton.onClick.AddListener(ChangeGameScene);
+    }
+
+    /// <summary>
+    /// GameScene へシーン遷移
+    /// </summary>
+    void ChangeGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
