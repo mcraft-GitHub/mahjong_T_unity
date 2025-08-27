bba65831baa7e0d9d4131d11aa08bfab72e23232
DESKTOP-Q8QHBTM\takag <takagi@m-craft.com>
2025-08-27 17:51:24 +0900

    ペア配置クラスの追加

---
+++ Assets/Scripts/PairPlacement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+/// <summary>
+/// ƒyƒA”z—ñî•ñ
+/// </summary>
+public class PairPlacement
+{
+    public Vector2Int a;
+    public Vector2Int b;
+    public string type;
+    public PairPlacement(Vector2Int a, Vector2Int b, string type) { this.a = a; this.b = b; this.type = type; }
+}
