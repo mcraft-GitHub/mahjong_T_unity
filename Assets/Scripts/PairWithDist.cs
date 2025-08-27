70d583e8e049c05f98a673c9accf0a1d12c3fea2
DESKTOP-Q8QHBTM\takag <takagi@m-craft.com>
2025-08-27 17:52:34 +0900

    距離付きペアクラスの追加

---
+++ Assets/Scripts/PairWithDist.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+/// <summary>
+/// ƒyƒA‚Æ‚»‚ÌÅ’Z‹——£‚ğ‚Ü‚Æ‚ß‚½\‘¢‘Ì
+/// </summary>
+public class PairWithDist
+{
+    public PairPlacement pair;
+    public int dist;
+    public PairWithDist(PairPlacement p, int d) { pair = p; dist = d; }
+}
