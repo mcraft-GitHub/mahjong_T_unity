81fd07aa6d219c10468abe101240fac1809d0f8c
DESKTOP-Q8QHBTM\takag <takagi@m-craft.com>
2025-08-27 10:13:38 +0900

    リザルトシーン・リザルトマネージャ・スコア保持クラスの追加

---
+++ Assets/Scripts/GameResultKeeper.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+/// <summary>
+/// リザルトの値を持っておくクラス
+/// </summary>
+public class GameResultKeeper : MonoBehaviour
+{
+    public static GameResultKeeper Instance 
+    {
+        get;
+        private set;
+    }
+
+    private float elapsed = 0.0f;
+    private int minutes = 0;
+    private int seconds = 0;
+    private float startTime;
+
+    void Start()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(this);
+
+        StartTime();
+    }
+
+    /// <summary>
+    /// 経過時間計算用 startTime の設定
+    /// </summary>
+    public void StartTime()
+    {
+        startTime = Time.time;
+    }
+
+    /// <summary>
+    /// 最終経過タイムの計算
+    /// GameScene から ResultScene へ遷移する際に使用
+    /// </summary>
+    public void HandleVictory()
+    {
+        elapsed = Time.time - startTime;
+        minutes = Mathf.FloorToInt(elapsed / 60f);
+        seconds = Mathf.FloorToInt(elapsed % 60f);
+    }
+
+    /// <summary>
+    /// 経過した分数を渡す
+    /// </summary>
+    /// <returns> 経過 分 </returns>
+    public int GetMinutes()
+    {
+        return minutes;
+    }
+
+    /// <summary>
+    /// 経過した秒数を渡す
+    /// </summary>
+    /// <returns> 経過 秒 </returns>
+    public int GetSeconds()
+    {
+        return seconds;
+    }
+}
