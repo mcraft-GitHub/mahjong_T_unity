73067dd527efd60bffc569d716c3a4bb2671c819
DESKTOP-Q8QHBTM\takag <takagi@m-craft.com>
2025-08-28 18:04:09 +0900

    タイトルシーンにフェード・点滅の実装

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -7,19 +8,29 @@ using UnityEngine.UI;
 /// </summary>
 public class TitleManager : MonoBehaviour
 {
+    [SerializeField] private FadeControl _fadeControl;
     [SerializeField] private Button _startButton;
 
     void Start()
     {
         if (_startButton != null)
-            _startButton.onClick.AddListener(ChangeGameScene);
+            _startButton.onClick.AddListener(OnStartButtonClicked);
+    }
+
+    /// <summary>
+    /// ラッパー関数
+    /// </summary>
+    private void OnStartButtonClicked()
+    {
+        StartCoroutine(ChangeGameScene());
     }
 
     /// <summary>
     /// GameScene へ シーン遷移
     /// </summary>
-    void ChangeGameScene()
+    private IEnumerator ChangeGameScene()
     {
+        yield return StartCoroutine(_fadeControl.FadeOutScene());
         SceneManager.LoadScene("GameScene");
     }
 }
