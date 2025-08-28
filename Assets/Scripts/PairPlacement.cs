3d66a4669248e37f5b9686b1ac00b2362a3ef1a0
DESKTOP-Q8QHBTM\takag <takagi@m-craft.com>
2025-08-28 10:57:07 +0900

    Assets/Scripts/PairPlacement.cs の 改行修正

--- Assets/Scripts/PairPlacement.cs
+++ Assets/Scripts/PairPlacement.cs
@@ -8,5 +8,10 @@ public class PairPlacement
     public Vector2Int _pairPlacementA;
     public Vector2Int _pairPlacementB;
     public string _type;
-    public PairPlacement(Vector2Int a, Vector2Int b, string type) { this._pairPlacementA = a; this._pairPlacementB = b; this._type = type; }
+    public PairPlacement(Vector2Int a, Vector2Int b, string type)
+    {
+        this._pairPlacementA = a;
+        this._pairPlacementB = b;
+        this._type = type;
+    }
 }
