d31d73c6c297cb7be2442534974a0b0c3c1f496e
DESKTOP-Q8QHBTM\takag <takagi@m-craft.com>
2025-09-08 17:33:06 +0900

    マウス入力検知・処理を行う新規スクリプト(前回間違えてプッシュしてしまいました)
    (cherry picked from commit 495f7eb7b63ae1c49fdc1ea5d3ce73a73e2aafb6)

--- Assets/Scripts/TileClickHandler.cs
+++ Assets/Scripts/TileClickHandler.cs
@@ -1,18 +1,68 @@
 using UnityEngine;
 
-public class TileClickHandler : MonoBehaviour
+/// <summary>
+/// マウス入力の検知,タイル選択・処理を行うクラス
+/// </summary>
+public class TileSelector : MonoBehaviour
 {
-    private GameManager _gm;
-    private Tile _tile;
+    private Camera _mainCamera;
+    private GameManager _gameManager;
+    private Tile _draggingTile;
 
-    public void Setup(GameManager gm, Tile tile)
+    private void Start()
     {
-        _gm = gm;
-        _tile = tile;
+        GameObject camObj = GameObject.Find("Main Camera");
+        _mainCamera = camObj.GetComponent<Camera>();
+        _gameManager = Object.FindAnyObjectByType<GameManager>();
     }
 
-    void OnMouseDown()
+    private void Update()
     {
-        _gm.OnTileClicked(_tile);
+        if (Input.GetMouseButtonDown(0)) // 左クリック
+        {
+            HandleClick();
+        }
+
+        if (Input.GetMouseButton(0)) // ドラッグ
+        {
+            HandleDrag();
+        }
+    }
+
+    /// <summary>
+    /// クリックされたタイルを取得し,GameManagerに通知
+    /// </summary>
+    private void HandleClick()
+    {
+        Tile tile = GetTileUnderMouse();
+        if (tile != null)
+        {
+            _gameManager.OnTileClicked(tile);
+        }
+    }
+
+    /// <summary>
+    /// ドラッグ中のタイルを取得し,GameManagerに通知
+    /// </summary>
+    private void HandleDrag()
+    {
+        Tile tile = GetTileUnderMouse();
+        if (tile == null || tile == _draggingTile) return;
+        _draggingTile = tile;
+    }
+
+    /// <summary>
+    /// ドラック中、カーソル下のタイルを取得
+    /// </summary>
+    /// <returns></returns>
+    private Tile GetTileUnderMouse()
+    {
+        Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            Tile tile = hit.collider.GetComponent<Tile>();
+            if (tile != null) return tile;
+        }
+        return null;
     }
 }
