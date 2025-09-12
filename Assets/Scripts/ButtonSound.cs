9e8507969dc35e718c19ad1be78098b8ddf32808
DESKTOP-Q8QHBTM\takag <takagi@m-craft.com>
2025-09-12 11:06:50 +0900

    サウンドマネージャーの追加・Buttonのサウンド再生共通用スクリプト追加

---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// ボタンを押した際、共通の音を鳴らす
+/// </summary>
+public class ButtonSound : MonoBehaviour
+{
+    private void Awake()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(() =>
+        {
+            SoundManager.Instance.PlaySE("SeConfirmClick");
+        });
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 音を管理するクラス
+/// </summary>
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance
+    {
+        get;
+        private set;
+    }
+    [Header("Audio Sources")]
+    [SerializeField] private AudioSource _bgmSource;
+    [SerializeField] private AudioSource _seSource;
+
+    [Header("Clips")]
+    [SerializeField] private AudioClip[] _bgmClips;
+    [SerializeField] private AudioClip[] _seClips;
+
+    private Dictionary<string, AudioClip> _bgmDict;
+    private Dictionary<string, AudioClip> _seDict;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // 辞書化
+        _bgmDict = new Dictionary<string, AudioClip>();
+        foreach (var clip in _bgmClips)
+        {
+            if (clip != null) _bgmDict[clip.name] = clip;
+        }
+
+        _seDict = new Dictionary<string, AudioClip>();
+        foreach (var clip in _seClips)
+        {
+            if (clip != null) _seDict[clip.name] = clip;
+        }
+    }
+
+    /// <summary>
+    /// bgmを再生する
+    /// </summary>
+    /// <param name="bgm"></param>
+    public void PlayBGM(string bgmName)
+    {
+        if (_bgmSource == null) return;
+        if (_bgmDict.TryGetValue(bgmName, out var clip))
+        {
+            _bgmSource.clip = clip;
+            _bgmSource.loop = true;
+            _bgmSource.Play();
+        }
+    }
+
+    /// <summary>
+    /// SEを再生する
+    /// </summary>
+    /// <param name="se"></param>
+    public void PlaySE(string seName)
+    {
+        if (_seSource == null || string.IsNullOrEmpty(seName)) return;
+
+        if (_seDict.TryGetValue(seName, out var clip))
+        {
+            _seSource.PlayOneShot(clip);
+        }
+    }
+}
